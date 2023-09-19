9d717e30c5ebb9624785c17f6986560a4cb4f849
Khaleda Akter <86180234+khaledamunni@users.noreply.github.com>
2023-09-19 12:42:08 +0600

    Update task-4.py

--- Lab-2 /task-4.py
+++ Lab-2 /task-4.py
@@ -1,1 +1,14 @@
 
+import cv2
+import numpy as np
+img = cv2.imread('a.jpeg',1)
+def draw(event,x,y,flags,params):
+    print("hii")
+cv2.namedWindow(winname="munni")
+cv2.setMouseCallback("munni",draw)
+while True:
+    cv2.imshow("img",img)
+    if cv2.waitKey(1) & 0xFF== ord('x'):
+        break
+
+cv2.destroyAllWindows()
